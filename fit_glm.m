894b921a551741f9a57c55d1b329eb2c3be29da8
shababo <benshababo@gmail.com>
2016-07-22 15:47:58 -0700

    testing glm

--- fit_glm.m
+++ fit_glm.m
@@ -1,60 +1,69 @@
 function fit_params = fit_glm(stims_x, stims_t, spikes)
 
-num_basis_funcs = 4;
+num_basis_funcs = 10;
 num_spatial_pos = 121;
 
-num_params = 1 + 2*(num_basis_funcs + 2) + num_spatial_pos;
+num_params = 1 + 2*num_basis_funcs + num_spatial_pos;
+
+bs = basisFactory.makeSmoothTemporalBasis('raised cosine', .750, num_basis_funcs, @(x) 1500);
+basis = bs.B';
 
 problem.solver = 'fminunc';
 problem.x0= zeros(num_params,1);
-problem.objective = @(x) glm_log_likelihood(x, stims_x, stims_t, spikes);
-problem.options = optimoptions('fminunc','Algorithm','quasi-newton','Display','iter');
+problem.objective = @(x) glm_log_likelihood(x, stims_x, stims_t, spikes, basis);
+problem.options = optimoptions('fminunc','Algorithm','quasi-newton',...
+                                'Display','iter','MaxFunEvals',5000);
 
+
+% delete(gcp('nocreate'))
+% this_pool = parpool();
 fit_params = fminunc(problem);
+% delete(this_pool)
 
 
-function log_likelihood = glm_log_likelihood(param_vec, stims_x, stims_t, spikes)
+function log_likelihood = glm_log_likelihood(param_vec, stims_x, stims_t, spikes, basis)
 
-num_basis_funcs = 4;
+num_basis_funcs = size(basis,1);
 
-% build the filters from basis function
-stim_filter = zeros(1,size(spikes,2));
-history_filter = zeros(1,size(spikes,2));
-
-t = (1:size(spikes,2))/20000;
 param_count = 1;
 baseline_rate = param_vec(param_count); param_count = param_count + 1;
 
 % build spatial layout of cell
 num_spatial_pos = 121;
-sptial_footprint = param_vec(param_count:param_count + num_spatial_pos-1);
+sptial_footprint = param_vec(param_count:param_count + num_spatial_pos - 1);
 param_count = param_count + num_spatial_pos;
 
-a_stim = param_vec(param_count); param_count = param_count + 1;
-c_stim = param_vec(param_count); param_count = param_count + 1;
+% a_stim = param_vec(param_count); param_count = param_count + 1;
+% c_stim = param_vec(param_count); param_count = param_count + 1;
 
-for i = 1:num_basis_funcs
-    phi = param_vec(param_count); param_count = param_count + 1;
-    stim_filter = stim_filter + raised_cosine(t,a_stim,c_stim,phi);
-end
+weights_stim = param_vec(param_count:param_count + num_basis_funcs - 1);
+param_count = param_count + num_basis_funcs;
+% for i = 1:num_basis_funcs
+% %     phi = param_vec(param_count); param_count = param_count + 1;
+%     weight =  param_vec(param_count); param_count = param_count + 1;
+%     stim_filter = stim_filter + weight * basis(i,:); %raised_cosine(t,a_stim,c_stim,phi);
+% end
+stim_filter = weights_stim'*basis;
 
-a_hist = param_vec(param_count); param_count = param_count + 1;
-c_hist = param_vec(param_count); param_count = param_count + 1;
-for i = 1:num_basis_funcs
-    phi = param_vec(param_count); param_count = param_count + 1;
-    history_filter = history_filter + raised_cosine(t,a_hist,c_hist,phi);
-end
+weights_hist = param_vec(param_count:param_count + num_basis_funcs - 1);
+param_count = param_count + num_basis_funcs;
+% a_hist = param_vec(param_count); param_count = param_count + 1;
+% c_hist = param_vec(param_count); param_count = param_count + 1;
+% for i = 1:num_basis_funcs
+% %     phi = param_vec(param_count); param_count = param_count + 1;
+%     weight =  param_vec(param_count); param_count = param_count + 1;
+%     history_filter = history_filter + weight * basis(i,:); %raised_cosine(t,a_hist,c_hist,phi);
+% end
+history_filter = weights_hist'*basis;
 
 linear_filter_out = baseline_rate*ones(size(spikes));
 
 for i = 1:size(stims_t,1)
     
     stim_filter_conv_out = conv(stims_t(i,:),stim_filter);
-    stim_filter_conv_out = stim_filter_conv_out(1:size(stims_t,2));
     stim_filter_conv_out = stim_filter_conv_out(1:size(spikes,2));
     
     hist_filter_conv_out = conv(spikes(i,:),history_filter);
-    hist_filter_conv_out = hist_filter_conv_out(1:size(stims_t,2));
     hist_filter_conv_out = hist_filter_conv_out(1:size(spikes,2));
     
     linear_filter_out(i,:) = linear_filter_out(i,:) + ...
--- test_glm.m
+++ test_glm.m
@@ -9,11 +9,11 @@ stims_t = repmat(stim_t,size(stims_x,1),1);
 
 spikes = zeros(size(stims_t));
 count = 1;
-for i = 1:4:6
+for i = 1:3
     for j = 1:11
         for k = 1:11
             for l = 1:5
-                spike_inds = find(detection_grids_3_31_s2c2_r2_3{i}{j,k}(l,1:1500));
+                spike_inds = find(all_detection_grids{1}{i}{j,k}(l,1:1500));
                 spikes(count,spike_inds) = 1;
                 count = count + 1;
             end
@@ -23,10 +23,15 @@ end
 
 %%
 
+
+
 fit_params = fit_glm(stims_x, stims_t, spikes);
 
+
 %%
-num_basis_funcs = 4;
+num_basis_funcs = 10;
+bs = basisFactory.makeSmoothTemporalBasis('raised cosine', .750, num_basis_funcs, @(x) 1500);
+basis = bs.B';
 
 % build the filters from basis function
 stim_filter = zeros(1,size(spikes,2));
@@ -42,19 +47,21 @@ num_spatial_pos = 121;
 sptial_footprint = fit_params(param_count:param_count + num_spatial_pos-1);
 param_count = param_count + num_spatial_pos;
 
-a_stim = fit_params(param_count); param_count = param_count + 1;
-c_stim = fit_params(param_count); param_count = param_count + 1;
+% a_stim = fit_params(param_count); param_count = param_count + 1;
+% c_stim = fit_params(param_count); param_count = param_count + 1;
 
 for i = 1:num_basis_funcs
-    phi = fit_params(param_count); param_count = param_count + 1;
-    stim_filter = stim_filter + raised_cosine(t,a_stim,c_stim,phi);
+%     phi = fit_params(param_count); param_count = param_count + 1;
+    weight =  fit_params(param_count); param_count = param_count + 1;
+    stim_filter = stim_filter + weight * basis(i,:); %raised_cosine(t,a_stim,c_stim,phi);
 end
 
-a_hist = fit_params(param_count); param_count = param_count + 1;
-c_hist = fit_params(param_count); param_count = param_count + 1;
+% a_hist = fit_params(param_count); param_count = param_count + 1;
+% c_hist = fit_params(param_count); param_count = param_count + 1;
 for i = 1:num_basis_funcs
-    phi = fit_params(param_count); param_count = param_count + 1;
-    history_filter = history_filter + raised_cosine(t,a_hist,c_hist,phi);
+%     phi = fit_params(param_count); param_count = param_count + 1;
+    weight =  fit_params(param_count); param_count = param_count + 1;
+    history_filter = history_filter + weight * basis(i,:); %raised_cosine(t,a_hist,c_hist,phi);
 end
 
 figure
