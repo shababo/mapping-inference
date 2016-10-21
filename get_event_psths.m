d86554be3cdbf850563b1be15e9746867f20a857
shababo <benshababo@gmail.com>
2016-10-20 19:02:59 -0700

    trying different temporal windows

--- get_event_psths.m
+++ get_event_psths.m
@@ -1,4 +1,4 @@
-function psth = get_event_psths(events,t)
+function psth = get_event_psths(events,t,amp_bounds)
 
 
 points = zeros(size(t));
@@ -8,10 +8,14 @@ for j = 1:length(events)
     if ~isempty(these_events)
         times = these_events(:,4);
         for i = 1:length(times)
-            ind = find(t < times(i),1,'last')
-            points(ind) = points(ind) + 1;
+            if times(i) > t(1) && times(i) < times(end) ...
+                    && these_events(i,1) > amp_bounds(1) && ...
+                    these_events(i,1) < amp_bounds(2)
+                ind = find(t < times(i),1,'last')
+                points(ind) = points(ind) + 1;
+            end
         end    
     end
 end
 points(end) = 0;
-psth = smoothts(points,'b',100)*100;
+psth = smoothts(points,'g',200,50)*100;
