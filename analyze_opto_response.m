3fdefd5986ad71a326ef100938876260f6322233
shababo <benshababo@gmail.com>
2017-04-20 07:28:53 -0700

    mas lif-glm

--- analyze_opto_response.m
+++ analyze_opto_response.m
@@ -1,37 +1,37 @@
-function cell = analyze_opto_response(cell)
+function this_cell = analyze_opto_response(this_cell)
 
-% cell is a struct like this
-% cell.filename
-% cell.start_inds... etc
+% this_cell is a struct like this
+% this_cell.filename
+% this_cell.start_inds... etc
 
-load(cell.filename)
+load(this_cell.filename)
 % package up and preprocess raw data
-start_trial = cell.start_trial;
-spike_thresh = cell.spike_thresh;
-num_spike_locs = cell.num_spike_locs;
-do_cc = cell.do_cc;
-do_vc = cell.do_vc;
-stim_start = cell.stim_start;
-cell_pos = cell.cell_pos;
-do_hpf = cell.hpass_filt
+start_trial = this_cell.start_trial;
+spike_thresh = this_cell.spike_thresh;
+num_spike_locs = this_cell.num_spike_locs;
+do_cc = this_cell.do_cc;
+do_vc = this_cell.do_vc;
+stim_start = this_cell.stim_start;
+cell_pos = this_cell.cell_pos;
+do_hpf = this_cell.hpass_filt
 
-[cell.spike_data, cell.voltage_data, cell.current_data] = ...
+[this_cell.spike_data, this_cell.voltage_data, this_cell.current_data, this_cell.intrinsics] = ...
     preprocess_opto_response(data,start_trial,spike_thresh,...
     num_spike_locs,do_cc,do_vc,stim_start,cell_pos);
 
 % do instrinsics
-% cell.intrinsics = analyze_fi_curve(data,cells.fi_trial); NOT IMPLEMENTED
+% this_cell.intrinsics = analyze_fi_curve(data,cells.fi_trial); NOT IMPLEMENTED
 % at least get V_rest
-if isfield(cell,'instrinsics')
-    cell.intrinsics.v_rest = median(cell.intrinsics.data(1:2*20000));
+if isfield(this_cell.intrinsics,'data')
+    this_cell.intrinsics.v_rest = median(this_cell.intrinsics.data(1:2*20000));
 end
     
 % run lif-glm
 downsamp = 1;
 distances = zeros(num_spike_locs,1);
 
-if isfield(cell.current_data,'current_shape')
-    current_template = cell.current_data.current_shape;
+if isfield(this_cell.current_data,'current_shape')
+    current_template = this_cell.current_data.current_shape;
 else
     load('chrome-template-3ms.mat','template');
     current_template = template(1:1001);
@@ -40,10 +40,10 @@ end
 count = 1;
 for k = 1:num_spike_locs
 
-    spike_times = cell.spike_data(k).spike_times;
-    powers = cell.spike_data(k).powers;
-    distances(k) = norm(cell.spike_data(k).location);
-    cell.spike_data(k).distance = distances(k);
+    spike_times = this_cell.spike_data(k).spike_times;
+    powers = this_cell.spike_data(k).powers;
+    distances(k) = norm(this_cell.spike_data(k).location);
+    this_cell.spike_data(k).distance = distances(k);
     for i = 1:length(powers)%-1 % don't do highest power - never useful
 
         for j = 1:length(spike_times{i})
@@ -59,34 +59,34 @@ for k = 1:num_spike_locs
 end
 
 g = [.01 .03 .05 .07 .09 .11 .13]*downsamp;
-cell.glm_params.g = g;
-cell.glm_params.downsamp = downsamp;
+this_cell.glm_params.g = g;
+this_cell.glm_params.downsamp = downsamp;
 
 devs = zeros(size(g));
 for i = 1:length(g)
     g(i)
     params.g = g(i);
-    [cell.glm_out(i).glm_result] = ...
+    [this_cell.glm_out(i).glm_result] = ...
         fit_lifglm(responses,stims,stims_ind,params);
-    cell.glm_out(i).dev = cell.glm_out(i).glm_result.dev;
+    this_cell.glm_out(i).dev = this_cell.glm_out(i).glm_result.dev;
 end
 
-[min_dev, min_ind] = min([cell.glm_out.dev]);
-cell.g = cell.glm_params.g(min_ind);
-this_glm_out = cell.glm_out(min_ind).glm_result;
-cell.v_th = this_glm_out.beta(1);
-cell.v_reset = this_glm_out.beta(2);
-cell.gain = this_glm_out.beta(3);
-cell.th_gain_ratio = this_glm_out.beta(1)/this_glm_out.beta(3);
+[min_dev, min_ind] = min([this_cell.glm_out.dev]);
+this_cell.g = this_cell.glm_params.g(min_ind);
+this_glm_out = this_cell.glm_out(min_ind).glm_result;
+this_cell.v_th = this_glm_out.beta(1);
+this_cell.v_reset = this_glm_out.beta(2);
+this_cell.gain = this_glm_out.beta(3);
+this_cell.th_gain_ratio = this_glm_out.beta(1)/this_glm_out.beta(3);
 
 sim_scale = 1;
-params_sim.V_th = cell.v_th*sim_scale;
-params_sim.V_reset = cell.v_reset*sim_scale;
+params_sim.V_th = this_cell.v_th*sim_scale;
+params_sim.V_reset = this_cell.v_reset*sim_scale;
 num_sim_trials = 50;
-params_sim.g = cell.g;
+params_sim.g = this_cell.g;
 funcs.invlink = @invlink_test;
-num_locs = length(cell.spike_data(k));
-num_powers = length(cell.spike_data(1).powers);
+num_locs = length(this_cell.spike_data(k));
+num_powers = length(this_cell.spike_data(1).powers);
 spike_count_means_glmfit_sim = zeros(num_locs,num_powers);
 spike_time_means_glmfit_sim = zeros(num_locs,num_powers);
 spike_time_std_glmfit_sim = zeros(num_locs,num_powers);
@@ -96,9 +96,9 @@ for k = 1:num_locs
 
     k
 
-    powers = cell.spike_data(k).powers;
+    powers = this_cell.spike_data(k).powers;
     params_sim.gain = ...
-        cell.glm_out(min_ind).glm_result.beta(k+2)*sim_scale;
+        this_cell.glm_out(min_ind).glm_result.beta(k+2)*sim_scale;
     for j = 1:length(powers)
         spike_times = [];
         sim_whole_cell = zeros(num_sim_trials,size(stims,2));
@@ -108,13 +108,13 @@ for k = 1:num_locs
             sim_whole_cell(i,:) = V_vect;
             spike_times = [spike_times find(spikes,1,'first')];
         end
-        cell.glm_sim(k).sim_whole_cell = sim_whole_cell;
+        this_cell.glm_sim(k).sim_whole_cell = sim_whole_cell;
         spike_count_means_glmfit_sim(k,j) = length(spike_times)/num_sim_trials;
         spike_time_means_glmfit_sim(k,j) = mean(spike_times);
         spike_time_std_glmfit_sim(k,j) = std(spike_times);
         
     end    
 end
-cell.glm_sim(k).spike_count_means = spike_count_means_glmfit_sim;
-cell.glm_sim(k).spike_time_means = spike_time_means_glmfit_sim;
-cell.glm_sim(k).spike_time_std = spike_time_std_glmfit_sim;
+this_cell.glm_sim(k).spike_count_means = spike_count_means_glmfit_sim;
+this_cell.glm_sim(k).spike_time_means = spike_time_means_glmfit_sim;
+this_cell.glm_sim(k).spike_time_std = spike_time_std_glmfit_sim;
--- test_opto_response_analysis.m
+++ test_opto_response_analysis.m
@@ -1,6 +1,6 @@
 
 base_cell.start_trial = 1;
-base_cell.spike_thresh = 5;
+base_cell.spike_thresh = 8;
 base_cell.num_spike_locs = 5;
 base_cell.do_cc = 0;
 base_cell.do_vc = 1;
@@ -12,80 +12,142 @@ base_cell.filename = '';
 base_cell.type = 'l23pyr';
 
 this_cell = 1;
-cell(this_cell) = base_cell;
-cell(this_cell).filename = '4_2_slice1_cell1.mat';
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_2_slice1_cell1.mat';
 base_cell.exclude_trials = [1 2 3];
 
 this_cell = 2; % not great location choices
-cell(this_cell) = base_cell;
-cell(this_cell).filename = '4_2_slice1_cell2.mat';
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_2_slice1_cell2.mat';
+base_cell.spike_thresh = 10;
 base_cell.exclude_trials = [1 4];
 
 this_cell = 3;
-cell(this_cell) = base_cell;
-cell(this_cell).filename = '4_2_slice2_cell1.mat';
-cell(this_cell).do_vc = 0;
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_2_slice2_cell1.mat';
+l23_cells(this_cell).do_vc = 0;
 base_cell.exclude_trials = [1 3];
 
 this_cell = 4;
-cell(this_cell) = base_cell;% really only looking at first spikes here
-cell(this_cell).filename = '4_2_slice2_cell2.mat';
-cell(this_cell).spike_thresh = 10;
+l23_cells(this_cell) = base_cell;% really only looking at first spikes here
+l23_cells(this_cell).filename = '4_2_slice2_cell2.mat';
+l23_cells(this_cell).spike_thresh = 10;
 base_cell.exclude_trials = [1 2 3 4 5];
 
 this_cell = 5; % really only looking at first spikes here
-cell(this_cell) = base_cell;
-cell(this_cell).filename = '4_2_slice2_cell3.mat';
-cell(this_cell).spike_thresh = 9;
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_2_slice2_cell3.mat';
+l23_cells(this_cell).spike_thresh = 7;
 base_cell.exclude_trials = [];
 
 this_cell = 6; % doublets
-cell(this_cell) = base_cell;
-cell(this_cell).filename = '4_2_slice3_cell1.mat';
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_2_slice3_cell1.mat';
 base_cell.exclude_trials = [1 4 5];
 
 this_cell = 7; % doublets
-cell(this_cell) = base_cell;
-cell(this_cell).filename = '4_2_slice3_cell1.mat';
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_2_slice3_cell2.mat';
+l23_cells(this_cell).spike_thresh = 7;
 % base_cell.exclude_trials = [1 4 5];
 
-% 
-% this_cell = 15; 
-% cell(this_cell) = base_cell;
-% cell(this_cell).filename = '4_3_slice3_cell2.mat';
-% cell(this_cell).type = 'l5pyr';
-% cell(this_cell).spike_thresh = 20;
-% 
-% this_cell = 16; 
-% cell(this_cell) = base_cell;
-% cell(this_cell).filename = '4_3_slice4_cell3.mat';
-% cell(this_cell).type = 'l5pyr';
-% 
-% this_cell = 17; 
-% cell(this_cell) = base_cell;
-% cell(this_cell).filename = '4_3_slice4_cell3.mat';
-% cell(this_cell).type = 'l5pyr';
-% cell(this_cell).spike_thresh = 12;
-% 
-% this_cell = 16; 
-% cell(this_cell) = base_cell;
-% cell(this_cell).filename = '4_6_slice4_cell3.mat';
-% cell(this_cell).type = 'l5pyr';
-% cell(this_cell).hpass_filt = 1;
+this_cell = 8;
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_2_slice4_cell1.mat';
+l23_cells(this_cell).spike_thresh = 12;
+
+this_cell = 9; % NO SPIKES - CONTROL CELL
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_2_slice4_cell2.mat';
+l23_cells(this_cell).spike_thresh = 50; % no spikes
+l23_cells(this_cell).do_vc = 0;
+base_cell.exclude_trials = [1 2 3 4 5];
+
+this_cell = 10;
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_3_slice1_cell1.mat';
+l23_cells(this_cell).start_trial = 3;
+
+this_cell = 11; %doublets
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_3_slice1_cell2.mat';
+l23_cells(this_cell).start_trial = 3;
+l23_cells(this_cell).do_vc = 0;
+
+this_cell = 12; 
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_3_slice1_cell3.mat';
+l23_cells(this_cell).start_trial = 2;
+l23_cells(this_cell).do_vc = 0;
+
+this_cell = 13; 
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_3_slice2_cell1.mat';
+l23_cells(this_cell).start_trial = 3;
+
+this_cell = 14; 
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_3_slice2_cell2.mat';
+
+this_cell = 15; 
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_3_slice3_cell1.mat';
+l23_cells(this_cell).spike_thresh = 11;
+
+%% l5 cells
+
+this_cell = 1; 
+l23_cells(this_cell) = base_cell;
+l23_cells(this_cell).filename = '4_3_slice3_cell2.mat';
+l23_cells(this_cell).spike_thresh = 16;
+
+this_cell = 2; 
+l5_cells(this_cell) = base_cell;
+l5_cells(this_cell).filename = '4_3_slice4_cell3.mat';
+
+this_cell = 3; 
+l5_cells(this_cell) = base_cell;
+l5_cells(this_cell).filename = '4_3_slice5_cell2.mat';
+
+this_cell = 4; 
+l5_cells(this_cell) = base_cell;
+l5_cells(this_cell).filename = '4_6_slice2_cell3.mat';
+l5_cells(this_cell).spike_thresh = 12;
+
+this_cell = 5; 
+l5_cells(this_cell) = base_cell;
+l5_cells(this_cell).filename = '4_6_slice3_cell3.mat';
+l5_cells(this_cell).hpass_filt = 1;l23_cells(this_cell).type = 'l5pyr';
+
+this_cell = 6; 
+l5_cells(this_cell) = base_cell;
+l5_cells(this_cell).filename = '4_6_slice3_cell4.mat';
+l5_cells(this_cell).hpass_filt = 1;
 
 
 
 %%
-cell_to_run = [1:6];
-cell_analyzed_bu = cell_analyzed;
-clear cell_analyzed
+cell_to_run = [9:15];
+% cell_analyzed_bu = cell_analyzed;
+% clear cell_analyzed
 for ii = 1:length(cell_to_run)
     this_cell = cell_to_run(ii)
-    cell_analyzed(this_cell) = analyze_opto_response(cell(this_cell));
+    l23_cell_analyzed(this_cell) = analyze_opto_response(l23_cells(this_cell));
     
 end
 
+%%
+cell_features = [[cell_analyzed.v_th]' [cell_analyzed.gain]' [cell_analyzed.g]'];
 
+figure; [~,ax] = plotmatrix(cell_features);
+ylabel(ax(1,1),'v-th')
+ylabel(ax(2,1),'optogain')
+ylabel(ax(3,1),'g')
+title(ax(1,1),'v-th')
+title(ax(1,1),'optogain')
+title(ax(1,1),'v-th')
+title(ax(1,2),'optogain')
+title(ax(1,3),'g')
 
 
 
