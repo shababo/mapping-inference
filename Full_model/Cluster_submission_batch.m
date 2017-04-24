c0c6c9da2326c9ff493a9655be29ac5a9305c30a
ChenShizhe <szchen@uw.edu>
2017-04-24 13:03:20 -0700

    Wrap up the functions

--- Full_model/Cluster_submission_batch.m
+++ Full_model/Cluster_submission_batch.m
@@ -14,6 +14,12 @@ random_prop_grid        = [0    0   0   0   0   0   0   0   0   0.2 0.4 0   0.2]
 num_random_grid         = [4    4   4   4   4   4   4   4   4   4   4   2   2]; % number of initial trials
 
 %%
+i_setting = ceil(arg1/50);
+arg1=mod(arg1, 50);
+if arg1 == 0
+	arg1=50;
+end
+%%
 for i_setting = 1:length(num_dense_grid)
     
     %---------------------------------------------------------------------%
@@ -26,6 +32,8 @@ for i_setting = 1:length(num_dense_grid)
     random_prop = random_prop_grid(i_setting);
     num_trials_batch=num_trials_batch_grid(i_setting);
     layer_feature=layer_feature_grid(i_setting);
+    
+    n_trial_update = 200;
     
     length_memory = num_trials_batch;
     num_peaks = 2*num_trials_batch;
@@ -76,17 +84,12 @@ for i_setting = 1:length(num_dense_grid)
     rng(arg1,'twister');
     
     % Parameters
-    %
     sqrt_transform = false; % whether to use squared transformation
-    
     % Parameters for the working model
     num_threshold=10; % number of bins to use
     mark = 0; % 0: amplitude; 1: latency.
     obj_function = @joint_sparsity_weight_entropy; %
     
-    
-    t_vect=0:data_params.dt:data_params.T;
-    V_vect=zeros(1,length(t_vect));
     num_trials_first =max(200, ceil(n_cell_local/num_sources)); % Number of trials in the first batches
     
     %---------------------------------------------------------------------%
@@ -104,31 +107,41 @@ for i_setting = 1:length(num_dense_grid)
         output(j).threshold = [];
     end
     output_ini = output;
-    X_g = zeros((N-1)*num_trials_batch + num_trials_first,n_cell_local);
-    locations_trials = zeros( (N-1)*num_trials_batch + num_trials_first,num_sources);
-    Y_g = zeros((N-1)*num_trials_batch + num_trials_first,num_threshold);
+    X_g = zeros(0,n_cell_local);
+    locations_trials = zeros(0,num_sources);
+    Y_g = zeros(0,num_threshold);
     % Initialize storage:
     output_crude =cell(N,1);
     time_record = zeros(N,1);
     
     % Conducting experiments
-    for t = 1:N
-        run('./Design/Design_LIF.m');
-        output(num_threshold).threshold= threshold;
-        output_crude{t}=output;
+    for i_batch = 1:N
+       tic
+       tstart=toc;
+    output_old=output;
+    [locations_this_batch, counts_freq] = optimal_design(i_batch, num_sources,num_peaks,num_trials_first,num_trials_batch, output, Y_g, ...
+    random_prop, counts_freq, pi_dense_local,inner_normalized_products, grid_index, freq_pen, num_samples);
+locations_trials = [locations_trials; locations_this_batch];
+[mpp_temp, ~, ~] = generate_data(...
+    locations_this_batch,pi_dense_all,k_offset,k_minimum,all_V_th,all_V_reset,all_gamma,...
+    all_amplitudes,all_sigma_across,all_sigma_within, ...
+    I_e_vect,g,stoc_mu,stoc_sigma, data_params,bg_params,trials_specific_variance);
+    
+[output, Y_g,X_g] = fit_working_model(...
+    i_batch,locations_this_batch,mpp_temp,Y_g,X_g,output_old,num_threshold,evoked_params,length_memory,k_offset,...
+    mark,pi_dense_local);
+tend=toc;
+        
+        
+        output_crude{i_batch}=output;
+        time_record(i_batch)=tend-tstart;
     end
-    stimuli_size_local = k_offset.*X_g;
-    
-   
-    
-    
-    
+
     %------------------------------------%
     % Estimating the marginal firing rate 
     sigma_unknown=1;
     I_stimuli = I_e_vect;
-    T=75;
-    dt=1;
+    T=75;dt=1;
     t_vect=0:dt:T;
     if layer_feature == 1
         V_thresholds = local_V_th_layer;
@@ -139,16 +152,11 @@ for i_setting = 1:length(num_dense_grid)
     end
     
     n_stimuli_grid=10;
-    n_trial = size(stimuli_size_local,1);
-    n_cell_local = size(stimuli_size_local,2);
-    % Prepare the batch data sets
-    estimated_intensity=cell(n_trial, n_cell_local);
+    [estimated_intensity]=Intensity(stimuli_size_local, n_stimuli_grid,n_grid_voltage,...
+        t_vect,t_factor,V_thresholds,V_resets,k_minimum,g,...
+        I_stimuli, stoc_mu, stoc_sigma);
+    
     evoked_cell = cell(n_trial,1);
-    
-    stimuli_temp = stimuli_size_local;
-    run('./Inference/Expected_intensity_v3.m');
-    estimated_intensity=M_intensity;
-    
     for i_trial = 1:n_trial
         evoked_cell_index = 0; % 0: background evnets
         for i_cell = 1:n_cell_local
@@ -166,7 +174,6 @@ for i_setting = 1:length(num_dense_grid)
         end
     end
     %---------------------------------------------------------------------%
-    
     
     convergence_epsilon = 0.01;
     maxit = 100;
@@ -190,13 +197,11 @@ for i_setting = 1:length(num_dense_grid)
         
         idx_trials_this_batch =  1: (num_trials_first + ...
             (t - 1)*num_trials_batch );
-        n_trial_update = max(idx_trials_this_batch);
+        
         n_trial_temp = max(idx_trials_this_batch);
         
-        
-        
-        %---------------------------------------------------------------------%
-    % Initialization of the full model 
+       %---------------------------------------------------------------------%
+       % Initialization of the full model 
         output= output_crude{t};
         overall_connectivity = zeros(n_cell_local,1);
         overall_mark = zeros(n_cell_local,1);
@@ -214,42 +219,36 @@ for i_setting = 1:length(num_dense_grid)
         
         %----------------------------------------------%
         % EM
-        gamma_old = overall_connectivity;
-        mu_old = overall_mark;
-        sigma_old = 1*ones(n_cell_local,1);
-        
         sparsity =0;
-        tstart = toc;
-        run('./Inference/Simulation_EM_batch.m');
-        tend=toc;
-        output_EM{ind_t}.sigma_samples = sigma_samples(:,end);
-        output_EM{ind_t}.gamma_samples = gamma_samples(:,end);
-        output_EM{ind_t}.mu_samples = mu_samples(:,end);
-        %output_EM{ind_t}.soft_assignments_samples= soft_assignments_samples;
-        output_EM{ind_t}.delta_t = tend-tstart;
+        [gamma_path mu_path sigma_path total_time]= ...
+            EM_fullmodel(mpp_new(1:n_trial_temp), estimated_intensity(1:n_trial_temp,:),evoked_cell,expected_all, ...
+              n_cell_local, overall_connectivity, overall_mark, ones(n_cell_local,1), ...
+        convergence_epsilon,f_background, mean_background, sigma_background, sparsity, gamma_threshold,maxit,t_vect);
+        %run('./Inference/Simulation_EM_batch.m');
+        output_EM{ind_t}.gamma = gamma_path(:,end);
+        output_EM{ind_t}.sigma = sigma_path(:,end);
+        output_EM{ind_t}.mu = mu_path(:,end);
+        output_EM{ind_t}.delta_t = total_time;
         %----------------------------------------------%
         
         %----------------------------------------------%
-        % EM with sparsity 
-        gamma_old = overall_connectivity;
-        mu_old = overall_mark;
-        sigma_old = 1*ones(n_cell_local,1);
-        
+        % EM with sparsity  
         sparsity =1;
-        tstart = toc;
-        run('./Inference/Simulation_EM_batch.m');
-        tend=toc;
-        output_sparse{ind_t}.sigma_samples = sigma_samples(:,end);
-        output_sparse{ind_t}.gamma_samples = gamma_samples(:,end);
-        output_sparse{ind_t}.mu_samples = mu_samples(:,end);
-        output_sparse{ind_t}.delta_t = tend-tstart;
+        [gamma_path mu_path sigma_path total_time]= ...
+            EM_fullmodel(mpp_new(1:n_trial_temp), estimated_intensity(1:n_trial_temp,:),evoked_cell,expected_all, ...
+             n_cell_local, overall_connectivity, overall_mark, ones(n_cell_local,1), ...
+        convergence_epsilon,f_background, mean_background, sigma_background, sparsity, gamma_threshold,maxit,t_vect);
+       output_sparse{ind_t}.gamma = gamma_path(:,end);
+        output_sparse{ind_t}.sigma = sigma_path(:,end);
+        output_sparse{ind_t}.mu = mu_path(:,end);
+        output_sparse{ind_t}.delta_t = total_time;
         %
         
         %--------------------------------------------------------%
         % Gibbs
         gamma_current = overall_connectivity;
         mu_current = overall_mark;
-        sigma_current =4*ones(n_cell_local,1);
+        sigma_current =1*ones(n_cell_local,1);
         %
         mu_m_hyper =overall_mark;
         mu_v_hyper = ones(n_cell_local,1); % inverse of prior variance
@@ -259,14 +258,17 @@ for i_setting = 1:length(num_dense_grid)
         gamma_alpha = ones(n_cell_local,1);
         gamma_beta = ones(n_cell_local,1);
         
-        
-        tstart = toc;
-        run('./Inference/Simulation_Gibbs_batch.m');
-        tend=toc;
-        output_Gibbs{ind_t}.sigma_samples = sigma_samples;
-        output_Gibbs{ind_t}.gamma_samples = gamma_samples;
-        output_Gibbs{ind_t}.mu_samples = mu_samples;
-        output_Gibbs{ind_t}.delta_t = tend-tstart;
+          [gamma_path mu_path sigma_path total_time]= ...
+            Gibbs_fullmodel(mpp_new(1:n_trial_temp), estimated_intensity(1:n_trial_temp,:),evoked_cell,expected_all, ...
+             n_trial_update, n_cell_local, overall_connectivity, overall_mark, ones(n_cell_local,1), ...
+              n_gibbs_sample,n_burnin,n_skip,...
+               mu_m_hyper, mu_v_hyper, sigma_alpha, sigma_beta, gamma_alpha, gamma_beta, ...
+        convergence_epsilon,f_background, mean_background, sigma_background, sparsity, gamma_threshold,maxit,t_vect);
+       output_Gibbs{ind_t}.gamma = gamma_path;
+        output_Gibbs{ind_t}.sigma = sigma_path;
+        output_Gibbs{ind_t}.mu = mu_path;
+        output_Gibbs{ind_t}.delta_t = total_time;
+      
     end
     
     % record data
@@ -274,7 +276,7 @@ for i_setting = 1:length(num_dense_grid)
     flnm = strcat('./Data/Setting',num2str(i_setting),'Design', num2str(design));
     
     save(strcat(flnm, 'Rep',num2str(arg1),'_Gibbs.mat'),'output_Gibbs');
-    save(strcat(flnm, 'Rep',num2str(arg1),'_Sparse.mat'),'output_Sparse');
+    save(strcat(flnm, 'Rep',num2str(arg1),'_Sparse.mat'),'output_sparse');
     save(strcat(flnm, 'Rep',num2str(arg1),'_EM.mat'),'output_EM');
     save(strcat(flnm, 'Rep',num2str(arg1),'_Data.mat'),'mpp_new','stimuli_size_local');
     save(strcat(flnm, 'Rep',num2str(arg1),'_Crude.mat'),'output_crude','time_record','threshold');
--- Full_model/Inference/Gibbs_fullmodel.m
+++ Full_model/Inference/Gibbs_fullmodel.m
@@ -1,52 +1,42 @@
-%gamma_ini = 0.5;
-gamma_current = overall_connectivity;
+% mpp: the marked point process
+% estimated_intensity: estimated marginal firing rate
+% evoked_cell: the list of cells evoked in each trial
+% convergence_epsilon: convergence threshold
+% mean_background: mean of background events
+% sigma_background: standard deviation of background events
+% sparsity: indicator for whether to encourage sparsity in the estimates
+% gamma_threshold: thresholds for sparse estimates
+function [gamma_path, mu_path, sigma_path, total_time] = Gibbs_fullmodel(mpp, estimated_intensity, evoked_cell,expected_all, ...
+    n_trial_update, n_cell_local, gamma_old, mu_old, sigma_old, ...
+    n_gibbs_sample,n_burnin,n_skip,...
+     mu_m_hyper, mu_v_hyper, sigma_alpha, sigma_beta, gamma_alpha, gamma_beta, ...
+    convergence_epsilon,f_background, mean_background, sigma_background, sparsity, gamma_threshold,maxit,t_vect)
+    tic;
+    tstart = toc;
+        
 
-mu_current = overall_mark;
-%sigma_current = evoked_params.sigma_a*ones(n_cell,1);
-sigma_current = 1*ones(n_cell_local,1);
-
-mu_m_hyper =overall_mark;
-mu_v_hyper = ones(n_cell_local,1); % inverse of prior variance 
-sigma_alpha = ones(n_cell_local,1);
-sigma_beta = ones(n_cell_local,1);
-
-gamma_alpha = overall_connectivity;
-gamma_beta = ones(n_cell_local,1);
-
-f_background = bg_params.firing_rate/1000; 
-w_background = 1/(bg_params.a_max -bg_params.a_min); % size distribution for background events
-%% 
-if exact_crossing == 1 
-    estimated_intensity = E_intensity;
-else 
-    estimated_intensity = M_intensity;
-end
-%% Precalculation:
-expected_all = zeros(i_trial,i_cell);
-for i_trial = 1:n_trial
-    for i_cell = 1:n_cell_local
-        expected_all(i_trial,i_cell)=sum(estimated_intensity{i_trial, i_cell} );
-    end
-end
-%% 
-mini_factor = n_trial/n_trial_update;
+    %% 
+    n_trial = size(mpp,2);
+    mini_factor = n_trial/n_trial_update;
         
 %% Run Gibbs sampler
-gamma_samples = zeros(n_gibbs_sample, n_cell_local);
-mu_samples = zeros(n_gibbs_sample, n_cell_local);
-sigma_samples = zeros(n_gibbs_sample, n_cell_local);
-soft_assignments_samples = cell(n_gibbs_sample,1);
+gamma_path = zeros(n_gibbs_sample, n_cell_local);
+mu_path = zeros(n_gibbs_sample, n_cell_local);
+sigma_path = zeros(n_gibbs_sample, n_cell_local);
 soft_assignments_current = cell(n_trial_update,1);
 i_sample = 0;
 i_counter = 0;
 while i_sample < n_gibbs_sample
     % Gibbs sampler
+     
+    % The E-step:
     %events_precell = cell(n_cell,1);
     events_precell = cell(n_cell_local,1);
+    trials_precell = cell(n_cell_local,1);
     for i_cell = 1:n_cell_local
         events_precell{i_cell} = [];
+        trials_precell{i_cell}=[];
     end
-    
     % Obtain the list of stimulated cells 
     evoked_cell_batch = [];
     chosen_trials_index = randsample(n_trial, n_trial_update);
@@ -61,52 +51,56 @@ while i_sample < n_gibbs_sample
     
     % Draw assignments
     % Pick a few trials
+     temp_ind = 0;
     for i_trial_index = 1:n_trial_update
         i_trial = chosen_trials_index(i_trial_index);
-        n_events = length(mpp_new(i_trial).event_times);
+        n_events = length(mpp(i_trial).event_times);
         evoked_cell_index = evoked_cell{i_trial};
         firing_rates = zeros(length(evoked_cell_index),1);
         size_rates = zeros(length(evoked_cell_index),1);
-        soft_assignments_current{i_trial_index}  = ones(n_events, length(evoked_cell_index))/length(evoked_cell_index);
+        soft_assignments{i_trial_index}  = ones(n_events, length(evoked_cell_index))/length(evoked_cell_index);
         
         for i_event = 1:n_events
-            this_event_time = mpp_new(i_trial).event_times(i_event);
-            this_event_size = mpp_new(i_trial).amplitudes(i_event);
+            this_event_time = mpp(i_trial).event_times(i_event);
+            this_event_size = mpp(i_trial).amplitudes(i_event);
             [~, i_t]=min(abs(t_vect - this_event_time));
                 
             for i_index = 1:length(evoked_cell_index)
                  i_cell=evoked_cell_index(i_index);
                 if i_cell== 0
                     firing_rates(i_index)  = f_background;
-                    size_rates(i_index) = w_background*(this_event_size>bg_params.a_min)*...
-                        (this_event_size<bg_params.a_max); 
+                    size_rates(i_index) =  normpdf(log(this_event_size),mean_background,sigma_background); 
                 else 
-                     firing_rates(i_index)  = gamma_current(i_cell)*estimated_intensity{i_trial,i_cell}(i_t);
-                    size_rates(i_index) = normpdf(this_event_size,mu_current(i_cell),sigma_current(i_cell));
+                     firing_rates(i_index)  = gamma_old(i_cell)*estimated_intensity{i_trial,i_cell}(i_t);
+                    size_rates(i_index) = normpdf(this_event_size,mu_old(i_cell),sigma_old(i_cell));
                 end
             end
             
             % Draw assignments given the estimated rates
-            %chances = firing_rates.*size_rates;
-            chances = firing_rates;
+            chances = firing_rates.*size_rates;
             if sum(chances)==0
                chances(1) = 1; 
             end
             chances = chances/sum(chances);
             %fprintf('%d', sum(chances));
-            soft_assignments_current{i_trial_index}(i_event,:) = chances; 
+            soft_assignments{i_trial_index}(i_event,:) = chances; 
+            if isnan( chances(1))
+                fprintf('Wrong')
+                fprintf('%d\n', i_cell)
+                temp_ind = 1;
+                break
+            end
         end
+       
         for i_index = 2:length(evoked_cell_index)
                 i_cell = evoked_cell_index(i_index);
-                events_precell{i_cell} = [events_precell{i_cell} [mpp_new(i_trial).amplitudes; soft_assignments_current{i_trial_index}(:,i_index)']];
+                events_precell{i_cell} = [events_precell{i_cell} [mpp(i_trial).amplitudes; soft_assignments{i_trial_index}(:,i_index)']];
+                trials_precell{i_cell} = [trials_precell{i_cell} i_trial];
         end 
     end
     
     %-------------------------------------------------------%
     % Draw mu and sigma 
-    if sigma_unknown == 1
-        %Not available...
-    else
         for i = 1:n_cell_evoked
             i_cell= evoked_cell_batch(i);
             % Sum of the weights:
@@ -117,17 +111,25 @@ while i_sample < n_gibbs_sample
                 post_mean = (mu_m_hyper(i_cell)*mu_v_hyper(i_cell)+mini_factor*weighted_sum(1))/...
                     (mu_v_hyper(i_cell)+ mini_factor*weighted_sum(2));
                 post_v = mu_v_hyper(i_cell)+mini_factor*weighted_sum(2);
+                post_sigma_alpha = sigma_alpha(i_cell)+mini_factor*weighted_sum(2)/2;
+                weighted_mean = weighted_sum(1)/weighted_sum(2);
+                
+                weighted_sigma = sum(events_precell{i_cell}(2,:).*((events_precell{i_cell}(1,:)-weighted_mean).^2));
+                post_sigma_beta = sigma_beta(i_cell)+ mini_factor*(weighted_sigma)/2+...
+                    ((weighted_mean-mu_m_hyper(i_cell))^2/2)*(mini_factor*weighted_sum(2)*mu_v_hyper(i_cell))/(mini_factor*weighted_sum(2)+mu_v_hyper(i_cell));
             else
                 post_mean = mu_m_hyper(i_cell);
                 post_v = mu_v_hyper(i_cell);
+                post_sigma_alpha = sigma_alpha(i_cell);
+                post_sigma_beta = sigma_beta(i_cell);
             end
-            % Draw mu:
+            sigma_current(i_cell) = 1/gamrnd(post_sigma_alpha, 1/post_sigma_beta);
             mu_current(i_cell) = normrnd(post_mean,sigma_current(i_cell)/sqrt(post_v) );
             else
                 % Do nothing
             end
         end
-    end
+    
     
     % Draw gamma
     % We take an approximated approach:
@@ -135,8 +137,8 @@ while i_sample < n_gibbs_sample
     %   2) Update the beta and gamma given the number of success
     %   3) Draw new gammas
     for i = 1:n_cell_evoked
-            i_cell= evoked_cell_batch(i);
-            if sum(size(events_precell{i_cell}))> 1
+        i_cell= evoked_cell_batch(i);
+        if sum(size(events_precell{i_cell}))> 1
             weighted_sum = sum(events_precell{i_cell},2);
             weighted_sum(1) = sum(events_precell{i_cell}(1,:).*events_precell{i_cell}(2,:));
             expected_events = sum( expected_all(chosen_trials_index,i_cell));
@@ -163,7 +165,7 @@ while i_sample < n_gibbs_sample
             else
                 gamma_current(i_cell) = betarnd(post_alpha,post_beta);
             end
-            end        
+        end
     end
     
     %----------------------------------------------%
@@ -172,17 +174,19 @@ while i_sample < n_gibbs_sample
     if i_counter > n_burnin 
         if mod(i_counter-1,n_skip) == 0  
             i_sample = i_sample +1;
-            gamma_samples(i_sample,:) = gamma_current;
-            mu_samples(i_sample,:) = mu_current;
-            sigma_samples(i_sample,:) = sigma_current;
+            gamma_path(i_sample,:) = gamma_current;
+            mu_path(i_sample,:) = mu_current;
+            sigma_path(i_sample,:) = sigma_current;
             
-            soft_assignments_samples{i_sample}  = soft_assignments_current;  
+            %soft_assignments_samples{i_sample}  = soft_assignments_current;  
         fprintf('%d\n',i_sample);
         end
     else
         fprintf('burnin %d\n',i_counter);
     end
     
+        tend=toc;
+        total_time = tend-tstart;
 end
 
 
