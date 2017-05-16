6630ac77580efafdaab40f63c28b3dc099dd6a36
shababo <benshababo@gmail.com>
2017-05-16 09:33:35 -0700

    commented lif-glm

--- lif-glm/fit_lifglm.m
+++ lif-glm/fit_lifglm.m
@@ -1,5 +1,13 @@
 function [stats_conv] = fit_lifglm(responses,stims,stims_ind,in_params)
 
+% responses is an N x T binary matrix of spike times where we have N trials
+% stims is an N x T matrix of the stimulus timeseries which is scaled by power - if using a shape to
+% fit the lif-glm then these should be scaled by the shape as well.
+% stims_ind is an N x 1 vector of location indices for each stimulus, this is if you are fitting
+% each location independently, if using ashape set all to 1
+% in_params is struct with the field g which is 1/tau for this neuron
+
+
 % in_params.link_type
 % in_params.fit_init
 % in_params.g
