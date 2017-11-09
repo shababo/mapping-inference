b29225c78956c30aface2086888bd967639086d3
Ben Shababo <benshababo@gmail.com>
2017-11-09 13:11:12 -0800

    added first design

--- Full_model/create_neighbourhoods.m
+++ Full_model/create_neighbourhoods.m
@@ -1,48 +1,48 @@
-function cell_neighbourhood = create_neighbourhoods(experiment_setup)
+function neighbourhoods = create_neighbourhoods(experiment_setup)
 
 % REDO WITH NEW STRUCTURES!!!!! NOT DONE YET
 if ~isfield(params.exp,'foe_bounds')
     params.exp.foe_bounds = [-148 148; -148 148];
 end
 
-cell_neighbourhood.cell_locations = cell_locations;
-cell_neighbourhood.z_locs = z_locs;
-cell_neighbourhood.n_cell = size(cell_locations,1);
+neighbourhoods.cell_locations = cell_locations;
+neighbourhoods.z_locs = z_locs;
+neighbourhoods.n_cell = size(cell_locations,1);
 
 % break up into z groups
 z_slice_width = params.exp.z_width;
 z_borders = [z_locs(1)-floor(z_slice_width/2); z_locs+floor(z_slice_width/2)];
 
-cell_neighbourhood.n_planes = length(z_locs);
+neighbourhoods.n_planes = length(z_locs);
 
-cell_group_idx = zeros(cell_neighbourhood.n_cell,1);
+cell_group_idx = zeros(neighbourhoods.n_cell,1);
 for i_cell = 1:size(cell_locations,1)
     cell_group_idx(i_cell)= sum(cell_locations(i_cell,3)>z_borders);
 end
-cell_neighbourhood.cell_group_list = cell(cell_neighbourhood.n_planes,1);
-for i_plane = 1:cell_neighbourhood.n_planes
-    cell_neighbourhood.cell_group_list{i_plane} = find(cell_group_idx==i_plane);
+neighbourhoods.cell_group_list = cell(neighbourhoods.n_planes,1);
+for i_plane = 1:neighbourhoods.n_planes
+    neighbourhoods.cell_group_list{i_plane} = find(cell_group_idx==i_plane);
 end
 
-assignin('base','cell_group_list',cell_neighbourhood.cell_group_list)
+assignin('base','cell_group_list',neighbourhoods.cell_group_list)
 assignin('base','cell_locations',cell_locations)
 
-pi_target_selected = cell(cell_neighbourhood.n_planes,1);
-inner_normalized_products = cell(cell_neighbourhood.n_planes,1);
-target_locations_selected = cell(cell_neighbourhood.n_planes,1);
-power_selected = cell(cell_neighbourhood.n_planes,1);
-target_locations_all = cell(cell_neighbourhood.n_planes,1);
-cell_neighbours = cell(cell_neighbourhood.n_planes,1);
-target_locations_nuclei = cell(cell_neighbourhood.n_planes,1);
-power_nuclei = cell(cell_neighbourhood.n_planes,1);
-pi_target_nuclei = cell(cell_neighbourhood.n_planes,1);
-loc_to_cell_nuclei = cell(cell_neighbourhood.n_planes,1);
-loc_to_cell = cell(cell_neighbourhood.n_planes,1);
+pi_target_selected = cell(neighbourhoods.n_planes,1);
+inner_normalized_products = cell(neighbourhoods.n_planes,1);
+target_locations_selected = cell(neighbourhoods.n_planes,1);
+power_selected = cell(neighbourhoods.n_planes,1);
+target_locations_all = cell(neighbourhoods.n_planes,1);
+cell_neighbours = cell(neighbourhoods.n_planes,1);
+target_locations_nuclei = cell(neighbourhoods.n_planes,1);
+power_nuclei = cell(neighbourhoods.n_planes,1);
+pi_target_nuclei = cell(neighbourhoods.n_planes,1);
+loc_to_cell_nuclei = cell(neighbourhoods.n_planes,1);
+loc_to_cell = cell(neighbourhoods.n_planes,1);
 
-for i = 1:cell_neighbourhood.n_planes
+for i = 1:neighbourhoods.n_planes
 
-    n_cell_this_plane = length(cell_neighbourhood.cell_group_list{i});
-    target_cell_list.primary = cell_neighbourhood.cell_group_list{i};
+    n_cell_this_plane = length(neighbourhoods.cell_group_list{i});
+    target_cell_list.primary = neighbourhoods.cell_group_list{i};
     target_cell_list.secondary = [];
     [pi_target_selected{i}, inner_normalized_products{i},targ_locs_tmp,...
     targ_loc_nuc_tmp, pi_target_nuclei{i}, loc_to_cell_nuclei{i}] = ...
@@ -73,10 +73,10 @@ end
 
 
 
-cell_neighbourhood.pi_target_selected = pi_target_selected;
-cell_neighbourhood.inner_normalized_products = inner_normalized_products;
-cell_neighbourhood.target_locations_selected = target_locations_selected;
-cell_neighbourhood.target_locations_nuclei = target_locations_nuclei;
-cell_neighbourhood.pi_target_nuclei = pi_target_nuclei;
-cell_neighbourhood.loc_to_cell_nuclei = loc_to_cell_nuclei;
-cell_neighbourhood.loc_to_cell = loc_to_cell;
+neighbourhoods.pi_target_selected = pi_target_selected;
+neighbourhoods.inner_normalized_products = inner_normalized_products;
+neighbourhoods.target_locations_selected = target_locations_selected;
+neighbourhoods.target_locations_nuclei = target_locations_nuclei;
+neighbourhoods.pi_target_nuclei = pi_target_nuclei;
+neighbourhoods.loc_to_cell_nuclei = loc_to_cell_nuclei;
+neighbourhoods.loc_to_cell = loc_to_cell;
---
+++ design_funcs/empty_design.m
@@ -0,0 +1,8 @@
+function experiment_query = empty_design(neighbourhood)
+
+experiment_query.neighbourhood_id = neighbourhood.neighbourhood_id;
+experiment_query.data_gen_type = 'acq';
+experiment_query.trials = [];
+experiment_query.batch_info.trial_counts = 0;
+experiment_query.batch_info.trial_counts = 0;
+experiment_query.batch_info.hologram_computing_time = NaN;
