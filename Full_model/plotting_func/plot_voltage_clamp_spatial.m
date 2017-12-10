f6d2d21664b86bf096f6d30983ebd2fe0811254e
Ben Shababo <shababo@airbears2-10-142-11-32.airbears2.1918.berkeley.edu>
2017-12-10 11:31:04 -0800

    plotting vclamp w detection spatially

--- Full_model/plotting_func/plot_voltage_clamp_spatial.m
+++ Full_model/plotting_func/plot_voltage_clamp_spatial.m
@@ -9,7 +9,6 @@ else
     dirpath = experiment_setup.analysis_root;
 end
     
-trials = struct();
 group_names = experiment_setup.group_names;
 
 % load batches and collect trials
@@ -21,37 +20,21 @@ for i = 1:length(batch_IDs)
                 
     load(batchsavepath)
     for j = 1:length(group_names)
-
-        trials = [trials experiment_query.(group_names{j}).trials];
+        
+        if isfield(experiment_query,group_names{j}) && ...
+                isfield(experiment_query.(group_names{j}),'trials') && ~isempty(experiment_query.(group_names{j}).trials)
+            if ~exist('trials','var')
+                trials = experiment_query.(group_names{j}).trials;
+            else
+                trials = [trials experiment_query.(group_names{j}).trials];
+            end
+        end
         
     end
                 
 end
+
+[vclamp_map, psc_time_map] = build_vclamp_grid(experiment_setup,trials,3);
     
-for i = 1%length(power_curve_num)
-    
-%     this_power = power_curve_num(i);
-    this_power = 0;
-    
-%     this_seq = this_seq(1:max_trial);
-    traces_pow{1} = traces_ch1(on_cell_trials' & [full_seq.target_power] == this_power,:);
-%     traces = [traces; traces_pow{1}];
-%     deorder = [deorder find(on_cell_trials' & [full_seq.target_power] == this_power)]; 
-    traces_pow{2} = traces_ch2(on_cell_trials' & [full_seq.target_power] == this_power,:);
-    this_seq_power = full_seq(on_cell_trials' & [full_seq.target_power] == this_power);
-    mpp_pow{i}{1} = mpp(on_cell_trials' & [full_seq.target_power] == this_power);
-    mpp_pow{i}{2} = mpp(on_cell_trials' & [full_seq.target_power] == this_power);
-%     color_these_trials{i}{1} = group_colors([this_seq_power.group],:);
-%     color_these_trials{i}{2} = group_colors([this_seq_power.group],:);
-%     mpp_pow{i} = mpp(num_trials+(1:length(this_seq_power)));
-%     mpp_pow{i}{1} = [];mpp_pow{i}{1} = [];
-    num_trials = num_trials + length(this_seq_power);
-    [maps_single{i}, mpp_maps{i}] = ...
-...%         see_grid_multi(traces_pow,mpp_pow{i},this_seq_power,full_stim_key,spacing,1,0,0);
-    see_grid_multi(traces_pow,mpp_pow{i},this_seq_power,full_stim_key,spacing,1,0,0);
-%     title(['Power = ' num2str(power_curve_num(i)) ' mW'])
-%     xlim(xlims); ylim(ylims);
-%     get_mean_events_image(mpp_maps{i}, 2000, 1, 1);
-%     title(['Event Counts, Power = ' num2str(power_curve_num(i)) ' mW'])
-%     caxis([0 2])
-end
+
+plot_trace_stack_grid(vclamp_map,Inf,1,0,[],[],[],psc_time_map);
