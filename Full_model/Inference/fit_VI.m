0d785dc9dc621f627ee8d2899f0c23e25cce1eb5
ChenShizhe <szchen@uw.edu>
2018-07-02 12:07:05 -0400

    minor updates

--- Full_model/Inference/fit_VI.m
+++ Full_model/Inference/fit_VI.m
@@ -2,22 +2,6 @@ function [parameter_history, loglklh_rec] = fit_VI(...
     trials,neurons, background_rate, ...
     variational_params,prior_params,...
     inference_params,prior_info,varargin)
-%% Stimulatin locations:
-
-% should include the stimulation locations in the mpp() data structure
-% also 
-% trials = experiment_query_this_group.trials;
-% neurons=this_neighbourhood.neurons;
-% Fit the delay for each cell:
-% %%
-% stim_size=designs_remained;
-% background_rate=bg_rate;
-% mpp=mpp_remained;
-% spike_indicator=false;
-%%
-%   background_rate=experiment_setup.patched_neuron.background_rate;
-% inference_params=  group_profile.inference_params;
-%  spike_indicator= false;
 %%
 
 if ~isempty(varargin) && ~isempty(varargin{1})
@@ -28,15 +12,15 @@ end
 % if ~isempty(varargin) && ~isempty(varargin{2})
 %    spike_indicator= varargin{2};
 % else
-    spike_indicator= false;
+spike_indicator= false;
 % end
 
 if zero_delay_indicator
-variational_params.alpha_s=-200;
-variational_params.alpha_m=-200;
-
-prior_params.alpha_s=-200;
-prior_params.alpha_m=-200;
+    variational_params.alpha_s=-200;
+    variational_params.alpha_m=-200;
+    
+    prior_params.alpha_s=-200;
+    prior_params.alpha_m=-200;
 end
 
 %%
@@ -67,11 +51,11 @@ eta_max=inference_params.step_size_max;
 maxit=inference_params.maxit;
 lklh_func=inference_params.likelihood;
 
-n_cell=size(stim_size,2);n_trial=size(stim_size,1);
+n_cell=length(neurons);
 
 Tmax=spike_curves.time_max;
 
-% initialize storages 
+% initialize storages
 sum_of_logs=zeros(S,1);logvariational=zeros(n_cell,S);
 logprior=zeros(n_cell,S);loglklh=zeros(n_cell,S);
 
@@ -87,14 +71,15 @@ gain_sample_mat=zeros(n_cell,S);
 delay_mu_sample_mat=zeros(n_cell,S);
 delay_sigma_sample_mat=zeros(n_cell,S);
 
-parameter_history=variational_params;
+%%
+boundary_params = prior_info.boundary_params;
 parameter_current=variational_params;
 
-% find relevant trials for each cell 
-relevant_trials = cell(n_cell,1);
-for i_cell = 1:n_cell
-    relevant_trials{i_cell}=find(stim_size(:,i_cell)>(minimum_stim_threshold/gain_bound.up));
-end
+% % find relevant trials for each cell
+% relevant_trials = cell(n_cell,1);
+% for i_cell = 1:n_cell
+%     relevant_trials{i_cell}=find(stim_size(:,i_cell)>(minimum_stim_threshold/gain_bound.up));
+% end
 iteration = 1;loglklh_rec=[];
 %%
 % tic;
@@ -103,134 +88,131 @@ while (change_history(iteration) > epsilon && iteration<maxit)
     %%
     parameter_history(iteration,:)=parameter_current;
     iteration=iteration+1;
-    
-     
+    log_prior_GP=zeros(n_cell,1);
+    adjusted_location=zeros(n_cell,3);
     for s= 1:S
-        % drawing samples 
-%         t1s=toc;
-
-[variational_samples,raw_samples] = draw_samples_from_var_dist(parameter_current);
-
-
-%% Calculate the stim size using samples from GP and the shifts
-
-shifts=[variational_samples(i_cell).shift_x variational_samples(i_cell).shift_y variational_samples(i_cell).shift_z];
-adjusted_location(i_cell,:)=neurons(i_cell).location-shifts;
-
-relevant_trials=[];
-relevant_stim_locations=zeros(0,3);
-trial_and_stim=zeros(0,2);
-
-this_adjusted_loc=adjusted_location(i_cell,:);
-for i_trial = 1:length(trials)
-    
-    relevant_flag = false;
-    for i_loc = 1:size(trials(i_trial).stim_location,1)
-        rel_position=trials(i_trial).stim_location(i_loc,:)-this_adjusted_loc;
-        this_relevant_flag =check_in_boundary(rel_position,boundary_params);
-        if  this_relevant_flag
-            relevant_stim_locations=[relevant_stim_locations; rel_position];
-            trial_and_stim=[trial_and_stim;  [i_trial i_loc]];
-            relevant_flag =true;
+        % drawing samples
+        [variational_samples,raw_samples] = draw_samples_from_var_dist(parameter_current);
+        %% Calculate the stim size using samples from GP and the shifts
+        for i_cell = 1:length(variational_samples) 
+        shifts=[variational_samples(i_cell).shift_x variational_samples(i_cell).shift_y variational_samples(i_cell).shift_z];
+        adjusted_location(i_cell,:)=neurons(i_cell).location-shifts;
+        
+        relevant_trials=[];
+        relevant_stim_locations=zeros(0,3);
+        trial_and_stim=zeros(0,2);
+        
+        this_adjusted_loc=adjusted_location(i_cell,:);
+        for i_trial = 1:length(trials)
+            relevant_flag = false;
+            for i_loc = 1:size(trials(i_trial).locations,1)
+                rel_position=trials(i_trial).locations(i_loc,:)-this_adjusted_loc;
+                this_relevant_flag =check_in_boundary(rel_position,boundary_params);
+                if  this_relevant_flag
+                    relevant_stim_locations=[relevant_stim_locations; rel_position];
+                    trial_and_stim=[trial_and_stim;  [i_trial i_loc]];
+                    relevant_flag =true;
+                end
+                if relevant_flag
+                relevant_trials = [relevant_trials i_trial];
+                end
+            end
         end
         
-    end
-end
-% Draw a GP on these positions:
-[this_cell_shape] = draw_3D_GP(relevant_stim_locations,n_shapes, GP_params);
-% 
-
-% Store the stim size in stim all
-for i_rel = 1:size(relavant_stim_locations,1)
-    i_trial = trial_and_stim(i_rel,1);
-    i_loc = trial_and_stim(i_rel,2);
-    stim_all(i_trial,i_cell)= trials(i_trial).power(i_loc)*this_cell_shape(i_rel);
-end
-log_prior_GP(i_cell)=this_cell_shape.loglklh;
+        % Draw a GP on these positions:
+        [this_cell_shape] = draw_3D_GP(relevant_stim_locations,n_shapes, GP_params);
+        
+        % Store the stim size in stim all
+        for i_rel = 1:size(relavant_stim_locations,1)
+            i_trial = trial_and_stim(i_rel,1);
+            i_loc = trial_and_stim(i_rel,2);
+            stim_all(i_trial,i_cell)= trials(i_trial).power(i_loc)*this_cell_shape(i_rel);
+        end
+        log_prior_GP(i_cell)=this_cell_shape.loglklh;
         
         
+        end
         
-        
-   %%     
+        %%
         
         % Draw samples from the GP on this corrected grid
         
-     logprior(s)=get_logdistribution(variational_samples,prior_params)+...
-         sum(log_prior_GP);
-      % NOTE: need to add the prior distribution of GP
-      
-%         ts=toc;
-%         % Calculate log pdf of prior and variational dist.
-%         logprior(:,s)=calculate_logpdf_spiked_logitnormal(prior_params,...
-%             logit_gamma,gamma_sample,logit_gain,gain_sample,...
-%             logit_delay_mu,delay_mu_sample,logit_delay_sigma,delay_sigma_sample,...
-%              delay_mu_bound,delay_sigma_bound,gamma_bound,gain_bound,spike_indicator);
-
-
-  logvariational(s)=get_logdistribution(variational_samples,parameter_current);
-      
-%         logvariational(:,s)=calculate_logpdf_spiked_logitnormal(parameter_current,...
-%             logit_gamma,gamma_sample,logit_gain,gain_sample,...
-%             logit_delay_mu,delay_mu_sample,logit_delay_sigma,delay_sigma_sample,...
-%              delay_mu_bound,delay_sigma_bound,gamma_bound,gain_bound);  
-%         te=toc;        time_rec(2)=time_rec(2)+te-ts;
+        logprior(s)=get_logdistribution(variational_samples,prior_params)+...
+            sum(log_prior_GP);
+        % NOTE: need to add the prior distribution of GP
         
-%         ts=toc;
-
-% Calcualte likelihood:
-
- [loglklh(s)] = update_likelihood(stim_size,mpp,...
-        gamma_sample_mat,gain_sample_mat,delay_mu_sample_mat,delay_sigma_sample_mat,...
+        %         ts=toc;
+        %         % Calculate log pdf of prior and variational dist.
+        %         logprior(:,s)=calculate_logpdf_spiked_logitnormal(prior_params,...
+        %             logit_gamma,gamma_sample,logit_gain,gain_sample,...
+        %             logit_delay_mu,delay_mu_sample,logit_delay_sigma,delay_sigma_sample,...
+        %              delay_mu_bound,delay_sigma_bound,gamma_bound,gain_bound,spike_indicator);
+        
+        
+        logvariational(s)=get_logdistribution(variational_samples,parameter_current);
+        
+        %         logvariational(:,s)=calculate_logpdf_spiked_logitnormal(parameter_current,...
+        %             logit_gamma,gamma_sample,logit_gain,gain_sample,...
+        %             logit_delay_mu,delay_mu_sample,logit_delay_sigma,delay_sigma_sample,...
+        %              delay_mu_bound,delay_sigma_bound,gamma_bound,gain_bound);
+        %         te=toc;        time_rec(2)=time_rec(2)+te-ts;
+        
+        %         ts=toc;
+        
+        % Calcualte likelihood:
+        
+        [loglklh(s)] = update_likelihood(stim_size,mpp,...
+            gamma_sample_mat,gain_sample_mat,delay_mu_sample_mat,delay_sigma_sample_mat,...
             minimum_stim_threshold,background_rate,relevant_trials,lklh_func,...
-        spike_curves,Tmax);
-    
-    
-lklhweight=logprior(s)+loglklh(s)-logvariational(s);
-
- this_gradient=get_variational_gradient(variational_samples,parameter_current);
+            spike_curves,Tmax);
+        
+        
+        lklhweight=logprior(s)+loglklh(s)-logvariational(s);
+        
+        this_gradient=get_variational_gradient(variational_samples,parameter_current);
         this_gradient=get_variate_control(lklhweight,this_gradient);
-       
-  if exist('gradients')
+        
+        if exist('gradients')
             gradients(s,:) = this_gradient;
         else
             gradients(s,:)=this_gradient;
-  end
-         
+        end
+        
     end
     
-      new_gradient=sum_gradient(gradients,eta,eta_max,iteration);
+    new_gradient=sum_gradient(gradients,eta,eta_max,iteration);
     
     [parameter_current, change_history(iteration)]=incorporate_gradient(parameter_current, new_gradient);
     
     loglklh_rec(iteration)=mean(mean(loglklh));
     elbo_rec(iteration)=mean(logprior+loglklh-logvariational);
-  
     
-        % Only thing that needs to change if we use the new way to get
-        % intensity: 
-%         ts=toc;
+    
+    % Only thing that needs to change if we use the new way to get
+    % intensity:
+    %         ts=toc;
     [loglklh] = update_likelihood(stim_size,mpp,...
         gamma_sample_mat,gain_sample_mat,delay_mu_sample_mat,delay_sigma_sample_mat,...
-            minimum_stim_threshold,background_rate,relevant_trials,lklh_func,...
+        minimum_stim_threshold,background_rate,relevant_trials,lklh_func,...
         spike_curves,Tmax);
-%         te=toc;        time_rec(4)=time_rec(4)+te-ts;
-% scatter(gain_sample_mat,loglklh)
-% ts=toc;
+    %         te=toc;        time_rec(4)=time_rec(4)+te-ts;
+    % scatter(gain_sample_mat,loglklh)
+    % ts=toc;
     [parameter_current, change_history(iteration)]= update_parameters_logitnormal(...
         parameter_current,loglklh, logprior, logvariational,...
         dqdp_logit, dqdalpha, dqdbeta, dqdalpha_gain, dqdbeta_gain,...
-         dqdalpha_m, dqdbeta_m, dqdalpha_s, dqdbeta_s,...
+        dqdalpha_m, dqdbeta_m, dqdalpha_s, dqdbeta_s,...
         iteration,eta,eta_max,spike_indicator);
-%         te=toc;        time_rec(5)=time_rec(5)+te-ts;
-  
-% parameter_current
-loglklh_rec(iteration)=mean(mean(loglklh));
-% change_history(iteration)=abs(loglklh_rec(iteration)-loglklh_rec(iteration-1))/abs(loglklh_rec(iteration));
-% parameter_current.alpha_m=-5;
-% parameter_current.beta_m=-1;
-% parameter_current.beta_s=-1;
-% parameter_current.alpha_s=-1;
-fprintf('Iteration %d; change %d; \n',iteration,change_history(iteration))
+    %         te=toc;        time_rec(5)=time_rec(5)+te-ts;
+    
+    % parameter_current
+    loglklh_rec(iteration)=mean(mean(loglklh));
+    % change_history(iteration)=abs(loglklh_rec(iteration)-loglklh_rec(iteration-1))/abs(loglklh_rec(iteration));
+    % parameter_current.alpha_m=-5;
+    % parameter_current.beta_m=-1;
+    % parameter_current.beta_s=-1;
+    % parameter_current.alpha_s=-1;
+    fprintf('Iteration %d; change %d; \n',iteration,change_history(iteration))
 end
 fprintf('VI fitted after %d iteration;\n',iteration)
-           
+
