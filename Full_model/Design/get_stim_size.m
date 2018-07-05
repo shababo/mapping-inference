daceedce96d74dda080dd71fc837da8792ca7143
ChenShizhe <szchen@uw.edu>
2018-07-05 02:11:36 -0400

    Improved psc simulation

--- Full_model/Design/get_stim_size.m
+++ Full_model/Design/get_stim_size.m
@@ -1,15 +1,17 @@
-function [stimuli_size] = get_stim_size(trials,this_neighbourhood,experiment_setup)
+function [stimuli_size] = get_stim_size(trials,neighbourhood,experiment_setup)
 %% Debug:
-% this_neighbourhood = neighbourhoods(1);
+% neighbourhood = neighbourhoods(1);
 % trials = experiment_query.undefined.trials;
 %% Dimensions 
 number_of_trials=length(trials);
-number_of_cells =length(this_neighbourhood.neurons);
-number_of_spots = size(trials(1).locations,2);
+number_of_cells =length(neighbourhood.neurons);
+number_of_spots = size(trials(1).locations,1);
 simulation_params=experiment_setup.sim;
+
+boundary_params = experiment_setup.prior_info.prior_parameters.boundary_params;
 %%
 stimuli_size=zeros(number_of_trials,number_of_cells);
-cell_ID_list=[this_neighbourhood.neurons(:).cell_ID];
+cell_ID_list=[neighbourhood.neurons(:).cell_ID];
 
 for l = 1:number_of_trials
     for m = 1:number_of_spots
@@ -18,13 +20,20 @@ for l = 1:number_of_trials
         if isnan(this_loc_ID)
         else
             this_loc_power =trials(l).power_levels(m);
-            cell_ID=trials(l).cell_IDs(m);
-            i_cell = find(cell_ID_list == cell_ID);
-            rel_loc = this_loc - this_neighbourhood.neurons(i_cell).truth.location;
-            if this_neighbourhood.neurons(i_cell).truth.PR>0
-            stimuli_size(l,:) = stimuli_size(l,:)+...
-                this_loc_power*griddata(simulation_params.mesh_grid(:,1),simulation_params.mesh_grid(:,2),simulation_params.mesh_grid(:,3),...
-                this_neighbourhood.neurons(i_cell).truth.shape,rel_loc(1),rel_loc(2),rel_loc(3));
+%             cell_ID=trials(l).cell_IDs(m);
+            for i_cell = 1:number_of_cells
+            
+            rel_loc = this_loc - neighbourhood.neurons(i_cell).truth.location;
+           
+            if (neighbourhood.neurons(i_cell).truth.PR & check_in_boundary(rel_loc, boundary_params))>0
+%             fprintf([num2str(l) '\n'])
+                this_size = griddata(simulation_params.mesh_grid(:,1),simulation_params.mesh_grid(:,2),simulation_params.mesh_grid(:,3),...
+                neighbourhood.neurons(i_cell).truth.shape,rel_loc(1),rel_loc(2),rel_loc(3));
+                if isnan(this_size)
+                   this_size = 0; 
+                end
+                stimuli_size(l,i_cell) = stimuli_size(l,i_cell)+ this_loc_power*this_size;
+            end
             end
         end
     end
--- design_funcs/get_undefined.m
+++ design_funcs/get_undefined.m
@@ -15,7 +15,7 @@ undefined_profile.design_func_params.candidate_grid_params.grid_type='ring'; % 2
 
 undefined_profile.design_func_params.trials_params=struct;
 undefined_profile.design_func_params.trials_params.replicates=1;
-undefined_profile.design_func_params.trials_params.spots_per_trial=3;
+undefined_profile.design_func_params.trials_params.spots_per_trial=1;
 undefined_profile.design_func_params.trials_per_cell=10;
 undefined_profile.design_func_params.trials_params.power_levels=50:10:100;
 undefined_profile.design_func_params.trials_params.stim_design='Nuclei';
