e2c6bf66c852408323331ac6b200fa0d757dc9d0
ChenShizhe <szchen@uw.edu>
2018-07-04 22:51:25 -0400

    Visualization of trials & outcomes

--- Full_model/Data_generation/draw_point_processes_intensity.m
+++ Full_model/Data_generation/draw_point_processes_intensity.m
@@ -1,5 +1,5 @@
 function [experiment_query_this_group] = draw_point_processes_intensity(experiment_query_this_group, ...
-        this_neighbourhood,experiment_setup)
+        neighbourhood,experiment_setup)
     
 %     
 %     
@@ -52,15 +52,15 @@ function [experiment_query_this_group] = draw_point_processes_intensity(experime
 % %% 
     
 time_max=length(experiment_setup.prior_info.current_template);
-number_of_cells=length(this_neighbourhood.neurons);
-cell_ID_list = [this_neighbourhood.neurons.cell_ID];
+number_of_cells=length(neighbourhood.neurons);
+cell_ID_list = [neighbourhood.neurons.cell_ID];
 number_of_trials=length(experiment_query_this_group.trials);
 % number_of_spots=length(experiment_query_this_group.trials(1).location_IDs);
 % stimuli_size=zeros(number_of_trials,number_of_cells);
 group_ID=experiment_query_this_group.trials(1).group_ID;
 
 [stimuli_size] = get_stim_size(experiment_query_this_group.trials,...
-    this_neighbourhood,experiment_setup);
+    neighbourhood,experiment_setup);
 % for l = 1:number_of_trials
 %     for m = 1:number_of_spots
 %         this_loc_ID=experiment_query_this_group.trials(l).location_IDs(m);
@@ -83,7 +83,7 @@ for i_trial = 1:number_of_trials
 
     for i_cell = 1:number_of_cells
         k=stimuli_size(i_trial,i_cell);
-        params_sim=this_neighbourhood.neurons(i_cell).truth;
+        params_sim=neighbourhood.neurons(i_cell).truth;
 %         params_sim.linkfuncs=experiment_setup.prior_info.induced_intensity.linkfunc;
         stim_threshold=experiment_setup.prior_info.induced_intensity.minimum_stim_threshold/experiment_setup.neurons(i_cell).truth.optical_gain;
         if k > stim_threshold
@@ -112,7 +112,7 @@ for i_trial = 1:number_of_trials
                             experiment_query_this_group.trials(i_trial).truth.spike_times=...
                [experiment_query_this_group.trials(i_trial).truth.event_times spikes(i)];
                             experiment_query_this_group.trials(i_trial).truth.assignments=...
-[experiment_query_this_group.trials(i_trial).truth.assignments this_neighbourhood.neurons(i_cell).cell_ID];
+[experiment_query_this_group.trials(i_trial).truth.assignments neighbourhood.neurons(i_cell).cell_ID];
 
                         end
                     end
--- Full_model/Data_generation/generate_psc_data.m
+++ Full_model/Data_generation/generate_psc_data.m
@@ -1,19 +1,19 @@
 function [experiment_query] = generate_psc_data(experiment_query,...
-    experiment_setup,this_neighbourhood)
-
+    experiment_setup,neighbourhood)
+%%
 group_names= fieldnames(experiment_query);
 for i_group = 1:length(group_names)
     
-    if any(get_group_inds(this_neighbourhood,group_names{i_group})) &&   isfield(experiment_query.(group_names{i_group}),'trials')
+    if any(get_group_inds(neighbourhood,group_names{i_group})) &&   isfield(experiment_query.(group_names{i_group}),'trials')
         
         if ~isempty(experiment_query.(group_names{i_group}).trials)
             experiment_query_this_group =experiment_query.(group_names{i_group});
             
             [experiment_query_this_group] = draw_point_processes_intensity(experiment_query_this_group, ...
-                this_neighbourhood,experiment_setup);
+                neighbourhood,experiment_setup);
             if experiment_setup.sim.sim_vclamp
                 experiment_query_this_group = gen_trace_from_mpp(experiment_query_this_group, ...
-                    this_neighbourhood,experiment_setup);
+                    neighbourhood,experiment_setup);
             end
             experiment_query.(group_names{i_group})=experiment_query_this_group;
         end
